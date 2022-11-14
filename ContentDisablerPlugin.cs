7108577531401c8d6667d4f6d0df432f5b8a24d1
William758 <williamreyes758@gmail.com>
2022-11-14 03:24:09 -0800

    Add files via upload

---
+++ ContentDisablerPlugin.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using RoR2;
+using RoR2.ContentManagement;
+
+using System.Security;
+using System.Security.Permissions;
+
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+
+namespace TPDespair.ContentDisabler
+{
+	[BepInPlugin(ModGuid, ModName, ModVer)]
+
+	public class ContentDisablerPlugin : BaseUnityPlugin
+	{
+		public const string ModVer = "1.0.0";
+		public const string ModName = "ContentDisabler";
+		public const string ModGuid = "com.TPDespair.ContentDisabler";
+
+		public static ConfigFile configFile;
+		public static ManualLogSource logSource;
+
+		public static Dictionary<string, int> ConfigKeys = new Dictionary<string, int>();
+
+
+
+		public void Awake()
+		{
+			configFile = Config;
+			logSource = Logger;
+
+			On.RoR2.ItemCatalog.Init += ItemCatalogInit;
+			On.RoR2.EquipmentCatalog.Init += EquipmentCatalogInit;
+		}
+
+
+
+		private static void ItemCatalogInit(On.RoR2.ItemCatalog.orig_Init orig)
+		{
+			foreach (ItemDef itemDef in ContentManager.itemDefs)
+			{
+				string name = itemDef.name;
+				if (name == "") name = itemDef.nameToken;
+				if (name == "") name = "UnknownItem";
+
+				ConfigEntry<bool> configEntry = ConfigEntry("Item", name, false, "Disable Item : " + name);
+				if (configEntry.Value)
+				{
+					itemDef._itemTierDef = null;
+					AssignDepricatedTier(itemDef, ItemTier.NoTier);
+					//itemDef.hidden = true;
+
+					LogWarn("Disabled Item : " + name);
+				}
+			}
+
+			orig();
+		}
+
+		private static void EquipmentCatalogInit(On.RoR2.EquipmentCatalog.orig_Init orig)
+		{
+			foreach (EquipmentDef equipDef in ContentManager.equipmentDefs)
+			{
+				string name = equipDef.name;
+				if (name == "") name = equipDef.nameToken;
+				if (name == "") name = "UnknownEquipment";
+
+				ConfigEntry<bool> configEntry = ConfigEntry("Equipment", name, false, "Disable Equipment : " + name);
+				if (configEntry.Value)
+				{
+					equipDef.canDrop = false;
+					equipDef.appearsInSinglePlayer = false;
+					equipDef.appearsInMultiPlayer = false;
+					equipDef.canBeRandomlyTriggered = false;
+					equipDef.enigmaCompatible = false;
+					equipDef.dropOnDeathChance = 0f;
+
+					LogWarn("Disabled Equipment : " + name);
+				}
+			}
+
+			orig();
+		}
+
+
+
+		internal static ConfigEntry<bool> ConfigEntry(string section, string key, bool defaultValue, string description)
+		{
+			string fullConfigKey = section + "_" + key;
+			string extra = ValidateConfigKey(fullConfigKey);
+			key += extra;
+			ConfigEntry<bool> configEntry = configFile.Bind(section, key, defaultValue, description);
+
+			return configEntry;
+		}
+
+		private static string ValidateConfigKey(string configKey)
+		{
+			if (!ConfigKeys.ContainsKey(configKey))
+			{
+				ConfigKeys.Add(configKey, 1);
+
+				return "";
+			}
+			else
+			{
+				LogWarn("ConfigEntry for " + configKey + " already exists!");
+
+				int value = ConfigKeys[configKey];
+
+				ConfigKeys[configKey] += 1;
+
+				return "_" + value;
+			}
+		}
+
+		internal static void AssignDepricatedTier(ItemDef itemDef, ItemTier itemTier)
+		{
+			#pragma warning disable CS0618 // Type or member is obsolete
+			itemDef.deprecatedTier = itemTier;
+			#pragma warning restore CS0618 // Type or member is obsolete
+		}
+
+
+
+
+		internal static void LogWarn(object data)
+		{
+			logSource.LogWarning(data);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# ContentDisabler
+
+Create a config for every item and equipment that allows hiding said item or equipment.
+
+This is a last resort for disabling items and equipment, you should use the config from source mod if available.
+
+Items and Equipment are removed from the logbook.
+
+Prevents items and equipment from showing up anywhere that draws from the normal drop pool :
+
+- Chests, Sacrifice, MultiShops, CommandMenus.
+
+Probably does not prevent items from showing up by other means (untested) :
+
+- HelperItems, FixedRewards, ItemTransformation, BossRewards
+
+## Installation:
+
+Requires Bepinex and HookGenPatcher.
+
+Use r2modman or place inside of Risk of Rain 2/Bepinex/Plugins/
+
+## Changelog:
+
+v1.0.0 - Initial Release.
